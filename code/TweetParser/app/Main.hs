9cf5cbd3249e7b5b6950b449fea718711decd2d4
Sridhar Ramesh <chinju@gmail.com>
2021-09-30 19:18:36 -0400

    Some string and file setup

--- code/TweetParser/app/Main.hs
+++ code/TweetParser/app/Main.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Prelude
 import Data.Aeson
+import qualified Data.ByteString.Lazy.UTF8 as UTF8
+import qualified Data.ByteString.Lazy as ByteString
+import Data.Attoparsec.ByteString.Lazy
+
+inputPath = "../../raw/TweetRecent.js"
+
+preludeParser = string "window.YTD.tweet.part0 = "
 
 main :: IO ()
-main = putStrLn "Hello world!!"
+main = do
+  file <- ByteString.readFile inputPath
+  putStrLn $ show $ ByteString.length file
+  let x = parse json file
+  case x of
+    Done _ _ -> putStrLn "Success"
+    Fail i x z -> putStrLn $ UTF8.toString $ ByteString.take 10 i
