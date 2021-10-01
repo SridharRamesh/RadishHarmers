1504416d76a15ac08f4393ed731bfbcccccde2e7
Sridhar Ramesh <chinju@gmail.com>
2021-10-01 16:28:36 -0400

    Uploading output HTML

--- code/TweetParser/src/Output.hs
+++ code/TweetParser/src/Output.hs
@@ -9,11 +9,15 @@ import qualified Data.String
 
 show x = Data.String.fromString $ Prelude.show x
 
+lineBreak = "\n<br>\n"
+
+paragraph x = "<p>\n" <> x <> "\n</p>\n"
+
 dateToText :: Date -> Text
 dateToText Date{..} = (show year) <> "-" <> (show month) <> "-" <> (show dayOfMonth)
 
 makeTweet :: Tweet -> Text
-makeTweet Tweet{..} = unlines
+makeTweet Tweet{..} = paragraph $ intercalate lineBreak
   ["Id: " <> fromStrict id,
    "Text: " <> fromStrict full_text,
    "Created at: " <> dateToText (date created_at),
@@ -23,7 +27,8 @@ makeTweet Tweet{..} = unlines
 pageHeader = unlines 
   ["<html>", 
    "<head>",
-   "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">"
+   "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">",
+   "<!-- This page was generated from TweetParser. -->"
   ]
 
 pageFooter = unlines
